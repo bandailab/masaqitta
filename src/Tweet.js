d96d1b41fd6dfba35db8ba0d95f2b1dac40ae486
destiny45109 <destiny45109@gmail.com>
2022-10-31 14:59:28 +0900

    ツイート表示画面のレイアウトを変更

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,23 +1,40 @@
 import {
   Box,
-} from "@chakra-ui/react";
+  Button,
+  ButtonGroup,
+  Spacer,
+  Stack,
+  HStack,
+  VStack,
+  Image,
+  Text,
+  useBreakpointValue,
+  Badge,
+} from '@chakra-ui/react'
 
 const Tweet = (props) => {
   return (
     <>
       <Box maxWidth={"container.sm"} border={"1px"}>
-        <Box>
-          {props.name}
-        </Box>
-        <Box>
-          {props.username}
-        </Box>
-        <Box>
-          {props.text}
-        </Box>
-        <Box>
-          {props.date}
-        </Box>
+        <HStack align='start'>
+          <Image
+            borderRadius='full'
+            height='60px'
+            // boxSize='60px'
+            marginX={"10px"}
+            marginY={"10px"}
+            src='https://bit.ly/dan-abramov'
+            alt='Dan Abramov'
+          />
+          <VStack spacing={3}>
+          <HStack justify={"start"} padding={0} margin={0} spacing={4} height='60px'>
+            <Box fontWeight={'extrabold'} fontSize={'15pt'}>{props.name}</Box>
+            <Box fontWeight={'bold'} fontSize={'15pt'}>{props.userName}</Box>
+            <Box fontWeight={'normal'} fontSize={'10pt'}>{props.date}</Box>
+          </HStack>
+          <Box padding={'15px'}>{props.text}</Box>
+        </VStack>
+      </HStack>
       </Box>
     </>
   );
